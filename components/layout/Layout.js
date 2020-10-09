df1fde9c3d49e15b0ec8dbce57cf20b82fb3d697
azahara <azaharamaga@gmail.com>
2020-10-09 20:01:24 +0200

    Create main layout component

---
+++ components/layout/Layout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+
+import GlobalTheme from './GlobalTheme';
+import Header from '../layout/Header';
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <GlobalTheme />
+
+      <Head>
+        <html lang='en' />
+        <title>Recipe Book</title>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+          integrity='sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=='
+          crossorigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Oxygen:wght@400;700&family=Source+Sans+Pro:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='stylesheet' href='/static/styles/app.css' />
+      </Head>
+
+      <Header />
+      <main>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
