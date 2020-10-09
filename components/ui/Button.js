8e723b8ce34f01cab4a8ee2aa28d3cb1721030b7
azahara <azaharamaga@gmail.com>
2020-10-09 20:02:43 +0200

    Define buttons styles

---
+++ components/ui/Button.js
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.a`
+  font-weight: 700;
+  text-transform: uppercase;
+  border: 1px solid #d1d1d1;
+  padding: 0.8rem 2rem;
+  margin-right: 1rem;
+  background-color: ${(props) => (props.bgColor ? '#00a896' : 'white')};
+  color: ${(props) => (props.bgColor ? 'white' : '#000')};
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 1.2rem 1.6rem 0 rgba(0, 0, 0, 0.24), 0 1.7rem 5rem 0 rgba(0, 0, 0, 0.19);
+  }
+`;
+
+export const ButtonSearch = styled.button`
+  height: 3rem;
+  width: 3rem;
+  display: block;
+  background-size: 4rem;
+  background-image: url('/static/image/search.png');
+  background-repeat: no-repeat;
+  position: absolute;
+  right: 1rem;
+  top: 1px;
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
