ac79b71c7f6f07e6ba913f36913da89d2932b93c
azahara <azaharamaga@gmail.com>
2020-10-10 21:05:43 +0200

    Use firebase in the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import App from 'next/app';
+import firebase, { FirebaseContext } from '../firebase';
+import useAuth from '../hooks/useAuth';
+
+const MyApp = ({ Component, pageProps }) => {
+  const user = useAuth();
+
+  return (
+    <FirebaseContext.Provider value={{ firebase, user }}>
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  );
+};
 
 export default MyApp;
