bc0f8707c248fb23bb49d5c86df58303b0bfe885
azahara <azaharamaga@gmail.com>
2020-10-10 14:12:15 +0200

    Use Form styles and delete styled component from this component

--- components/ui/SearchBox.js
+++ components/ui/SearchBox.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
 import { ButtonSearch } from './Button';
-
-const InputText = styled.input`
-  border: 1px solid var(--greyLight);
-  border-radius: 7px;
-  padding: 1rem;
-  min-width: 300px;
-  outline: none;
-  &:focus {
-    border-color: var(--blueDark02);
-    box-shadow: 0 0 10px var(--blueDark02);
-  }
-`;
-
-const InputSubmit = styled.button``;
+import { Input } from './Form';
 
 const SearchBox = () => {
   return (
@@ -25,7 +11,13 @@ const SearchBox = () => {
         position: relative;
       `}
     >
-      <InputText type='text' placeholder='Search Recipes' />
+      <Input
+        css={css`
+          min-width: 300px;
+        `}
+        type='text'
+        placeholder='Search Recipes'
+      />
       <ButtonSearch title='search' type='submit' />
       <ButtonSearch />
     </form>
