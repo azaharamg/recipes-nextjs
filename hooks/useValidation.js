4f2dc2d42265fa8e0b57613f5348f857e4638cee
azahara <azaharamaga@gmail.com>
2020-10-10 14:12:47 +0200

    Create a custom hook

---
+++ hooks/useValidation.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+const useValidation = (initialState, validation, actionFn) => {
+  const [values, storeValues] = useState(initialState);
+  const [errors, storeErrors] = useState({});
+  const [submitForm, storeSubmitForm] = useState(false);
+
+  useEffect(() => {
+    if (submitForm) {
+      const success = Object.keys(errors).length === 0;
+
+      if (success) {
+        actionFn();
+      }
+      storeSubmitForm(false);
+    }
+  }, [errors]);
+
+  const handleChange = (e) => {
+    storeValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationError = validation(values);
+    storeErrors(validationError);
+    storeSubmitForm(true);
+  };
+
+  const handleBlur = () => {
+    const validationError = validation(values);
+    storeErrors(validationError);
+  };
+
+  return {
+    values,
+    errors,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  };
+};
+
+export default useValidation;
