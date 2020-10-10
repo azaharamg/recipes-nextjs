ff75cebfb8e82e72f0eff646593a57922fd00731
azahara <azaharamaga@gmail.com>
2020-10-10 21:05:13 +0200

    create firebase instance

---
+++ firebase/firebase.js
@@ -0,0 +1,38 @@
+import app from 'firebase/app';
+import firebaseConfig from './config';
+import 'firebase/auth';
+
+class Firebase {
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(firebaseConfig);
+    }
+    this.auth = app.auth();
+  }
+
+  /**
+   * Register a new user
+   */
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(email, password);
+
+    return await newUser.user.updateProfile({ displayName: name });
+  }
+
+  /**
+   * User login
+   */
+  async login(email, password) {
+    return await this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  /**
+   * User log out
+   */
+  async logOut() {
+    return await this.auth.signOut();
+  }
+}
+
+const firebase = new Firebase();
+export default firebase;
