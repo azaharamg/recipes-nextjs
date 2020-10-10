72337ab912f7b8ec4b0c0512e90768b375784757
azahara <azaharamaga@gmail.com>
2020-10-10 21:03:46 +0200

    Create Login

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
+import { css } from '@emotion/core';
+
+import firebase from '../firebase';
+
 import Layout from '../components/layout/Layout';
+import { Form, Field, InputSubmit, Input, Error } from '../components/ui/Form';
+
+import useValidation from '../hooks/useValidation';
+import validateLogin from '../validation/validateLogin';
+
+import { STATE_INITIAL } from '../constants/constants';
 
 const Login = () => {
+  const [loginError, storeError] = useState(false);
+
+  const { values, errors, handleSubmit, handleChange, handleBlur } = useValidation(
+    STATE_INITIAL,
+    validateLogin,
+    userLogin
+  );
+
+  const { email, password } = values;
+
+  async function userLogin() {
+    try {
+      await firebase.login(email, password);
+      Router.push('/');
+    } catch (error) {
+      console.log('Login failed for user');
+      storeError(error.message);
+    }
+  }
+
   return (
     <div>
       <Layout>
-        <h1>Login</h1>
+        <React.Fragment>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Login
+          </h1>
+          <Form onSubmit={handleSubmit} noValidate>
+            <Field>
+              <label htmlFor='email'>Email</label>
+              <Input
+                type='email'
+                id='email'
+                placeholder='Introduce your email'
+                name='email'
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.email && <Error>{errors.email}*</Error>}
+            </Field>
+            <Field>
+              <label htmlFor='password'>Password</label>
+              <Input
+                type='password'
+                id='password'
+                placeholder='Write your password'
+                name='password'
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.password && <Error>{errors.password}*</Error>}
+            </Field>
+
+            {loginError && (
+              <Error
+                css={css`
+                  text-align: center;
+                `}
+              >
+                {`Sorry, ${loginError}`}
+              </Error>
+            )}
+
+            <InputSubmit type='submit' value='Login' />
+          </Form>
+        </React.Fragment>
       </Layout>
     </div>
   );
---
+++ validation/validateLogin.js
@@ -0,0 +1,17 @@
+export default function validateLogin(values) {
+  let errors = {};
+
+  if (!values.email) {
+    errors.email = 'Email is required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i) {
+    errors.email = 'Email is not valid!';
+  }
+
+  if (!values.password) {
+    errors.password = 'Password is required';
+  } else if (values.password.length < 6) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  return errors;
+}
