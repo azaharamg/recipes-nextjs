f2b4a708b4a7f8804115491c3100f06de08dd9bf
azahara <azaharamaga@gmail.com>
2020-10-12 11:14:35 +0200

    Get data from database and pass it to RecipeDetails component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../firebase';
 import Layout from '../components/layout/Layout';
+import RecipeDetails from '../components/layout/RecipeDetails';
 
 const Home = () => {
+  const [recipes, storeRecipes] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  function manageSnapshot(snapshot) {
+    const recipes = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    storeRecipes(recipes);
+  }
+
+  useEffect(() => {
+    const getRecipes = () => {
+      firebase.db.collection('recipes').orderBy('published', 'desc').onSnapshot(manageSnapshot);
+    };
+
+    getRecipes();
+  }, []);
+
   return (
     <div>
       <Layout>
-        <h1>Home</h1>
+        <div className='recipes-list'>
+          <div className='container'>
+            <ul className='bg-white'>
+              {recipes.map((recipe) => (
+                <RecipeDetails key={recipe.id} recipe={recipe} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
