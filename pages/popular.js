660ad4dbf812367b4161f19b5ae66266c968e1c8
azahara <azaharamaga@gmail.com>
2020-10-12 18:51:06 +0200

    Create popular page section

--- pages/popular.js
+++ pages/popular.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import useRecipes from '../hooks/useRecipes';
 import Layout from '../components/layout/Layout';
+import RecipeDetails from '../components/layout/RecipeDetails';
 
 const Popular = () => {
+  const { recipes } = useRecipes('votes');
+
   return (
     <div>
       <Layout>
-        <h1>Most Popular</h1>
+        <div className='recipes-list'>
+          <div className='container'>
+            <ul className='bg-white'>
+              {recipes.map((recipe) => (
+                <RecipeDetails key={recipe.id} recipe={recipe} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
