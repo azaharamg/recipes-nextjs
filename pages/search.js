a357d6fe1262af99898c889f5b17b078040d8c1f
azahara <azaharamaga@gmail.com>
2020-11-01 12:41:25 +0100

    Pass users's search to the search component to show the filtered information

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import useRecipes from '../hooks/useRecipes';
 import Layout from '../components/layout/Layout';
+import RecipeDetails from '../components/layout/RecipeDetails';
 
 const Search = () => {
+  const [result, setResult] = useState([]);
+
+  const router = useRouter();
+  const {
+    query: { q },
+  } = router;
+
+  const { recipes } = useRecipes('published');
+
+  useEffect(() => {
+    const search = q.toLowerCase();
+    const searchedRecipe = recipes.filter((recipe) => {
+      return recipe.name.toLowerCase().includes(search) || recipe.description.toLowerCase().includes(search);
+    });
+    setResult(searchedRecipe);
+  }, [q, recipes]);
+
   return (
     <div>
       <Layout>
-        <h1>Search</h1>
+        <div className='recipes-list'>
+          <div className='container'>
+            <ul className='bg-white'>
+              {result.map((recipe) => (
+                <RecipeDetails key={recipe.id} recipe={recipe} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
