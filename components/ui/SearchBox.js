7b2ac2d018f4d9d535f7f55588e76a8604092701
azahara <azaharamaga@gmail.com>
2020-11-01 12:23:42 +0100

    Get users's search to redirect them to the path '/search'

--- components/ui/SearchBox.js
+++ components/ui/SearchBox.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
 import { css } from '@emotion/core';
 
 import { ButtonSearch } from './Button';
 import { Input } from './Form';
 
 const SearchBox = () => {
+  const [search, storeSearch] = useState('');
+
+  const searchRecipe = (e) => {
+    e.preventDefault();
+
+    if (typeof search !== 'string' || search.trim() === '') return;
+    Router.push({
+      pathname: '/search',
+      query: { q: search },
+    });
+  };
+
   return (
     <form
       css={css`
         position: relative;
       `}
+      onSubmit={searchRecipe}
     >
       <Input
         css={css`
@@ -17,6 +31,7 @@ const SearchBox = () => {
         `}
         type='text'
         placeholder='Search Recipes'
+        onChange={(e) => storeSearch(e.target.value)}
       />
       <ButtonSearch title='search' type='submit' />
       <ButtonSearch />
