970d8319f5f7caeb33d1513face409b59d981c52
azahara <azaharamaga@gmail.com>
2020-11-01 15:18:39 +0100

    Enable 404 component to be customizable

--- components/layout/404.js
+++ components/layout/404.js
@@ -13,14 +13,20 @@ const Image = styled.img`
   height: 50vh;
 `;
 
-const Error404 = () => {
+const Error404 = (props) => {
+  const { title, description } = props;
   return (
     <Container>
-      <h1>Oops!!</h1>
-      <p>We can't seem to find the page you're looking for.</p>
+      <h1>{title}</h1>
+      <p>{description}</p>
       <Image src='/static/image/404.svg' alt='404' />
     </Container>
   );
+};
+
+Error404.defaultProps = {
+  title: 'Oops!!',
+  description: "We can't seem to find the page you're looking for.",
 };
 
 export default Error404;
